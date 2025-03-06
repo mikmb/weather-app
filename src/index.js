0fd75eb95d34dc39fb3ac9c5e054ec3713061163
mikmb <1057416+mikmb@users.noreply.github.com>
2025-03-05 23:08:55 -0800

    Added webpack and did some clean up.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+import "./styles/styles.css";
+
+import clearDay from "./assets/clear-day.svg";
+import clearNight from "./assets/clear-night.svg";
+import cloudy from "./assets/cloudy.svg";
+import fog from "./assets/fog.svg";
+import hail from "./assets/hail.svg";
+import partlyCloudyDay from "./assets/partly-cloudy-day.svg";
+import partlyCloudyNight from "./assets/partly-cloudy-night.svg";
+import rainSnowShowersDay from "./assets/rain-snow-showers-day.svg";
+import rainSnowShowersNight from "./assets/rain-snow-showers-night.svg";
+import rainSnow from "./assets/rain-snow.svg";
+import rain from "./assets/rain.svg";
+import showersDay from "./assets/showers-day.svg";
+import showersNight from "./assets/showers-night.svg";
+import sleet from "./assets/sleet.svg";
+import snowShowersDay from "./assets/snow-showers-day.svg";
+import snowShowersNight from "./assets/snow-showers-night.svg";
+import snow from "./assets/snow.svg";
+import thunderRain from "./assets/thunder-rain.svg";
+import thunderShowersDay from "./assets/thunder-showers-day.svg";
+import thunderShowersNight from "./assets/thunder-showers-night.svg";
+import thunder from "./assets/thunder.svg";
+import wind from "./assets/wind.svg";
+
+const imageIcons = {
+  cloudy: cloudy,
+  fog: fog,
+  hail: hail,
+  rain: rain,
+  sleet: sleet,
+  snow: snow,
+  thunder: thunder,
+  wind: wind,
+  "clear-day": clearDay,
+  "clear-night": clearNight,
+  "partly-cloudy-day": partlyCloudyDay,
+  "partly-cloudy-night": partlyCloudyNight,
+  "rain-snow-showers-day": rainSnowShowersDay,
+  "rain-snow-showers-night": rainSnowShowersNight,
+  "rain-snow": rainSnow,
+  "showers-day": showersDay,
+  "showers-night": showersNight,
+  "snow-showers-day": snowShowersDay,
+  "snow-showers-night": snowShowersNight,
+  "thunder-rain": thunderRain,
+  "thunder-showers-day": thunderShowersDay,
+  "thunder-showers-night": thunderShowersNight,
+};
+
 const API_KEY = "N252VEE3PNULMBJ4QWGZQEJ9D";
 const BASE_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
@@ -20,10 +70,6 @@ fahrenheitButton.addEventListener("click", () => {
   useMetric = false;
   updateTempsDisplay(currentWeather);
 });
-
-function celsiusToFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
-}
 
 function fahrenheitToCelsius(fahrenheit) {
   return (((fahrenheit - 32) * 5) / 9).toFixed(1);
@@ -64,7 +110,7 @@ function updateTempsDisplay(currentWeather) {
   tempDiv.textContent = temp + ` ${tempUnit}`;
 
   const conditionIcon = document.querySelector(".condition-icon");
-  conditionIcon.src = `../assets/${currentWeather.icon}.svg`;
+  conditionIcon.src = imageIcons[`${currentWeather.icon}`];
 
   const conditionText = document.querySelector(".condition-text");
   conditionText.textContent = currentWeather.conditions;
